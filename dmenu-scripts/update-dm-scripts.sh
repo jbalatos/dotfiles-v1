937028bef4ee611a462fcd23d543d07f7c4be79f
Jim Balatos <jbalatos@gmail.com>
2022-03-07 19:13:19 +0200

    bug on update-dm-scripts

--- dmenu-scripts/update-dm-scripts.sh
+++ dmenu-scripts/update-dm-scripts.sh
@@ -6,3 +6,5 @@ sudo cp dm-logout /usr/local/bin
 echo "Added dm-logout to /usr/local/bin"
 sudo cp dm-windows /usr/local/bin
 echo "Added dm-windows to /usr/local/bin"
+sudo cp edit /usr/local/bin
+echo "Added edit to /usr/local/bin"
